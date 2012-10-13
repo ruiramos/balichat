4a22084d68c5d04c34f757a6864de93027c65217
Tiago Pinto <thpinto@gmail.com>
2012-10-13 01:18:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balichat::Application.config.secret_token = '1e6c16601fdb6cad5d4b1f388198d00238783decc46ce9db35647a0fb4e690145f11fb13ea3acd9533629941f3b308069796a1d36be4f3ca65180b428473b904'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Balichat::Application.config.session_store :cookie_store, key: '_balichat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Balichat::Application.config.session_store :active_record_store
