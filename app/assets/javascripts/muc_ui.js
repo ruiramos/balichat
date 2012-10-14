bbf80fdf1c517659411a607a33afdde3c84a6400
Ricardo Otero <oterosantos@gmail.com>
2012-10-14 04:23:53 +0100

    Lorena

--- app/assets/javascripts/muc_ui.js
+++ app/assets/javascripts/muc_ui.js
@@ -42,8 +42,6 @@ MucUi.fn.appendMessage = function(nick, message) {
   var from = $(message).attr('from');
   var text = $(message).find('body').text();
 
-  console.debug(text);
-
   // FIXME: POR ISTO NOUTRO SITIO
   if(text.match(/(?:^|\s)https?:\/\//)){
     text = text.replace(/(?:^|\s)https?:\/\/(?:www.)?vimeo.com\/(\d*)(?:$|\s)/,'<iframe src="http://player.vimeo.com/video/$1?title=1&amp;byline=1&amp;portrait=1" width="500" height="377" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
